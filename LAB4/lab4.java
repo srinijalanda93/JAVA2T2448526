21fef84ccfc90028061d0c25a4b727fb0e75c012
srinijalanda93 <srinijalanda@gmail.com>
2024-11-16 10:21:07 +0530

    Lab4

---
+++ LAB4/lab4.java
@@ -0,0 +1,14 @@
+package LAB4;
+
+abstract class Robber{
+    //cann't create the object for abstract class
+    //Partially completed --> because object cann't be created
+    
+}
+
+public class lab4 {
+    public static void main(String[] args){
+        System.out.println("Abstract Concepts");
+    }
+
+}
